b789814eece554a3deb8e6de029590a4de0539bf
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-06-02 13:27:00 +0700

    Add function to create file & Test

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/marttp/grpc-go-tutorial
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.1.1
+	github.com/stretchr/testify v1.6.0
+	gitlab.com/techschool/pcbook v0.0.0-20200530140618-343f6ae8e43a // indirect
+	google.golang.org/protobuf v1.24.0
+)
---
+++ serializer/file_test.go
@@ -0,0 +1,32 @@
+package serializer_test
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/marttp/grpc-go-tutorial/pb"
+	"github.com/marttp/grpc-go-tutorial/sample"
+	"github.com/marttp/grpc-go-tutorial/serializer"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileSerializer(t *testing.T) {
+	t.Parallel()
+
+	binaryFile := "../tmp/laptop.bin"
+	jsonFile := "../tmp/laptop.json"
+
+	laptop1 := sample.NewLaptop()
+
+	err := serializer.WriteProtobufToBinaryFile(laptop1, binaryFile)
+	require.NoError(t, err)
+
+	err = serializer.WriteProtobufToJSONFile(laptop1, jsonFile)
+	require.NoError(t, err)
+
+	laptop2 := &pb.Laptop{}
+	err = serializer.ReadProtobufFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+
+	require.True(t, proto.Equal(laptop1, laptop2))
+}
